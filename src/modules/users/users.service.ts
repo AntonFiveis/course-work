9eba4d532f5d3696021321e6aa3ec92d8972da24
Michael Medvediev <misha.medvedev2001@gmail.com>
2020-12-22 19:14:17 +0200

    Some changes

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -5,32 +5,32 @@ import { UsersEntity } from './users.entity';
 import * as bcrypt from 'bcrypt'
 @Injectable()
 export class UsersService {
-  constructor(private pgService:PgService) {}
+  constructor(private pgService: PgService) { }
   private tableName = "Users"
-  async getUserByEmail(email:string){
+  async getUserByEmail(email: string) {
     const res = await this.pgService.useQuery(`SELECT * FROM "Users" WHERE "email"='${email}'`)
     return res.rows[0]
   }
-  async createUser({email,firstName,lastName,patronymic,phone,password}:UsersDto){
+  async createUser({ email, firstName, lastName, patronymic, phone, password }: UsersDto) {
     const salt = await bcrypt.genSalt()
-    const genPassword = await bcrypt.hash(password,salt)
+    const genPassword = await bcrypt.hash(password, salt)
     return this.pgService.useQuery(`SELECT "CreateUser" ('${email}','${firstName}','${lastName}','${patronymic}','${phone}','${genPassword}','${salt}')`)
   }
-  async updateUser(userID:number,updates:UsersUpdates){
-    return this.pgService.update({tableName:this.tableName,updates, where:`"userID"=${userID}`})
+  async updateUser(userID: number, updates: UsersUpdates) {
+    return this.pgService.update({ tableName: this.tableName, updates, where: `"userID"=${userID}` })
   }
-  async deleteUser(userID:number){
-    await this.pgService.delete({tableName:this.tableName,where:`"userID"=${userID}`})
+  async deleteUser(userID: number) {
+    await this.pgService.delete({ tableName: this.tableName, where: `"userID"=${userID}` })
   }
-  async signIn(email,password){
-    const user: UsersEntity =await this.getUserByEmail(email)
-    if(this.validatePassword(password,user)){
+  async signIn(email: string, password: string) {
+    const user: UsersEntity = await this.getUserByEmail(email)
+    if (this.validatePassword(password, user)) {
       return user
-    }else{
+    } else {
       throw new UnauthorizedException("Incorrect credentials")
     }
   }
-  async validatePassword(login_password:string,{password,salt}:UsersEntity){
+  async validatePassword(login_password: string, { password, salt }: UsersEntity) {
     return await bcrypt.hash(login_password, salt) === password;
   }
 }
